a86ecf6335fdbe0b0a3cf1ca100e0ebf09ddb836
JulianaDomenech <38088048+JulianaDomenech@users.noreply.github.com>
2022-06-27 15:22:46 -0500

    Output json results to screen
    Output json results to screen in a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # this writes the data that is returned out on the screen
+
+#take the json version of the response and normalize it
+fruitvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it to the screen
+streamlit.dataframe(fruitvice_normalized )
