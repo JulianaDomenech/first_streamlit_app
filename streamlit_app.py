2dba2ffeed485d90ec755b212a5c2e084ad2ef47
JulianaDomenech <38088048+JulianaDomenech@users.noreply.github.com>
2022-06-28 16:30:08 -0500

    fixing spaces
    fixing spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,10 +58,10 @@ def get_fruit_load_list():
          return my_cur.fetchall()
          
 #Add a button to load the fruit
-   if streamlit.button('Get Fruit Load List'):
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-      my_data_rows = get_fruit_load_list()
-      streamlit.dataframe(my_data_rows)
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 #Allow user to add fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you to add?','jackfruit')
